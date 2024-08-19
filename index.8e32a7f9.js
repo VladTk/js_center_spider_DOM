f15f22d285fccdfaa043590e316a66bbb44c423b
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-19 18:06:29 +0300

    make build

---
+++ index.8e32a7f9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,o=t.offsetWidth,i=t.offsetHeight;t.style.top="".concat(l/2-i/2,"px"),t.style.left="".concat(c/2-o/2,"px");
+//# sourceMappingURL=index.8e32a7f9.js.map
